d58a403b7b63ef31ce92ced9d3ef0e69d731fade
Ryan Breen <ryan@ryanbreen.com>
2018-11-24 15:08:26 -0500

    Day 5, part 2

--- day5/index.js
+++ day5/index.js
@@ -17,7 +17,18 @@ const part1 = input => {
 }
 
 const part2 = input => {
+  const jumps = _.map(fs.readFileSync(input).toString().split("\n"), i => { return parseInt(i) });
 
+  let pc = 0
+  let counter = 0
+  while (pc >= 0 && pc < jumps.length) {
+    const jumpDistance = jumps[pc]
+    jumps[pc] = jumpDistance >= 3 ? jumpDistance - 1 : jumpDistance + 1
+    pc += jumpDistance
+    counter += 1
+  }
+
+  return counter
 }
 
 const input = './day5/input'
