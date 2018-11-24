8f1dde48d00f18b711d2d8bd78bffaebcb6bdfeb
Ryan Breen <ryan@ryanbreen.com>
2018-11-24 13:31:55 -0500

    Day 4, part 1

---
+++ day4/index.js
@@ -0,0 +1,30 @@
+const _ = require('lodash')
+const fs = require('fs');
+
+const part1 = input => {
+  const lines = fs.readFileSync(input).toString().split("\n");
+  return _.reduce(
+    _.map(lines, line => {
+      let entries = line.split(" ")
+      return entries.length == _.uniq(entries).length
+    }),
+    (sum, n) => {return sum + n},
+    0
+  )
+}
+
+const part2 = input => {
+}
+
+const input = './day4/input'
+
+module.exports = {
+  part1: {
+    exec: part1,
+    input: input
+  },
+  part2: {
+    exec: part2,
+    input: input
+  }
+}
